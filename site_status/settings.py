452972b85bdfebe1daee0a7d4e1fdc788b539dd8
Victor Magalhaes <victor.barreto@buser.com.br>
2022-07-08 12:41:01 -0300

    sera

--- site_status/settings.py
+++ site_status/settings.py
@@ -155,3 +155,19 @@ INTERNAL_IPS = [
 ]
 
 django_on_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
