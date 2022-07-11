1383e613588eea9fcd9a70872b53823df4e81fbe
Victor Magalhaes <victor.barreto@buser.com.br>
2022-07-11 18:05:45 -0300

    Model atualizado

--- core/models.py
+++ core/models.py
@@ -5,8 +5,13 @@ from django.conf import settings
 class Site(models.Model):
     url = models.URLField(max_length=512)
     name = models.CharField(max_length=128)
-    is_active = models.BooleanField(default=True)
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
 
     def __str__(self) -> str:
         return self.name
+
+class SiteStatus(models.Model):
+    request_code = models.CharField(max_length=16)
+    is_active = models.BooleanField(default=True)
+    last_check = models.DateTimeField()
+    site_id = models.ForeignKey('Site', on_delete=models.CASCADE)
