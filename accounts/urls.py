deaa9046050ada607bb9fe943da2d5d6a56123f8
Beatriz <beatrizramalho.esteves@gmail.com>
2022-07-14 14:27:54 -0300

    cadastro

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,18 @@
+from django.views.generic import TemplateView
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+from . import forms
 
 urlpatterns = [
-    path('register/', views.SignUp.as_view(), name='signup')
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="registration/login.html",
+            authentication_form=forms.LoginForm,
+        ),
+        name="login",
+    ),
+    path('register/', views.SignUp.as_view(), name='signup'),
+    #path('logout/', auth_views.LoginView.as_view(), name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import UsuarioForm
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = UsuarioForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
