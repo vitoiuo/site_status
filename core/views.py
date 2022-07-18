81603fc90e20bcc8713de6ff97aeb80bc99a3a87
Victor Magalhaes <victor.barreto@buser.com.br>
2022-07-18 13:50:28 -0300

    Importação das views arrumadas

--- core/views.py
+++ core/views.py
@@ -1,5 +1,3 @@
-from this import d
-from turtle import down
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from django.contrib.auth.models import User
